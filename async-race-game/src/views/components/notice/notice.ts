f2fdeb9986f99eead3b9340552696bc5f510d861
SK-website <vnali@tutanota.com>
2021-10-23 17:12:37 +0300

    ref: add project files from school repo

--- async-race-game/src/views/components/notice/notice.ts
+++ async-race-game/src/views/components/notice/notice.ts
@@ -4,6 +4,6 @@ import { BaseComponent } from '../base-component';
 export class Notice extends BaseComponent {
   constructor(name: string, time: number) {
     super('div', ['notice', 'hidden']);
-    this.element.textContent = `The ${name} won has won the race with time ${time / 1000}s!`;
+    this.element.textContent = `${name} has won the race with time ${time / 1000}s!`;
   }
 }
